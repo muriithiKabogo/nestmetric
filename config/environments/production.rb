c06c7d660b17f06032d8da0b005329f892061bba
muriithiKabogo <dmuriithi.k@gmail.com>
2017-06-16 23:15:37 +0000

    adds sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'https://f432cdef.ngrok.io' }
+  config.action_mailer.default_url_options = { :host => 'https://secret-crag-97102.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   :user_name            => ENV['SENDGRID_USERNAME'],
