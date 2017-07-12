2c8433f709d0aab758138bb6862932c0f1e3ad03
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-12 14:48:38 +0000

    adds to stripe/webhook user mailer and user mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
 
-	def suscription_payment_failed(user,customer_email, template, esubject)
+	def suscription_payment_failed(userAcount,user,customer_email, template, esubject)
 
 		@url  = 'http://example.com/login'
 	    @customer_email = customer_email
 	    @template = template
-	  
+	  	@uid = userAcount
 
 	    
 	    mail from: user,
