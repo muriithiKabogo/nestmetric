34e7ede7e0436ec83e2a196c96f9218374622f7e
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-12 13:59:07 +0000

    resolved error in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
 
-	def suscription_payment_failed(,user,customer_email, template, esubject)
+	def suscription_payment_failed(user,customer_email, template, esubject)
 
 		@url  = 'http://example.com/login'
 	    @customer_email = customer_email
