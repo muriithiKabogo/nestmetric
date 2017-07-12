bbbdaa55a561ed438e89bd6dd6464f1fb9a47e5d
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-12 13:48:24 +0000

    adds login page as png

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
 
-	def suscription_payment_failed(userAccount,user,customer_email, template, esubject)
+	def suscription_payment_failed(,user,customer_email, template, esubject)
 
 		@url  = 'http://example.com/login'
 	    @customer_email = customer_email
 	    @template = template
-	    @uid = userAccount
+	  
 
 	    
 	    mail from: user,
