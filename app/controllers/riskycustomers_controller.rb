7b5e78c20479acd36777bdc42c02a535106a594e
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-13 13:40:51 +0000

    tries to identify the email

--- app/controllers/riskycustomers_controller.rb
+++ app/controllers/riskycustomers_controller.rb
@@ -8,11 +8,11 @@ class RiskycustomersController < ApplicationController
   def new
   	 @user = User.find_by_uid(params[:uid])
 
-  	 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-  	 @publishable_key = ENV['stripe_publishable_key'] 
-  	 customer_email = Stripe::Customer.retrieve(params[:custId])
-     @customer_email = customer_email[:email]
-     puts @customer_email
+  	 # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+  	 # @publishable_key = ENV['stripe_publishable_key'] 
+  	 # @customer_email = Stripe::Customer.retrieve(params[:custId])
+    #  @customer_email = @customer_email[:email]
+    #  puts @customer_email.class
   end
 
   def create
