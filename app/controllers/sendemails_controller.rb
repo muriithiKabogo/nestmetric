84f9ad200519a26ad5bbd5048a2f5082cf3a58e1
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-20 11:58:11 +0000

    corrects the send mail action create

--- app/controllers/sendemails_controller.rb
+++ app/controllers/sendemails_controller.rb
@@ -2,21 +2,21 @@ class SendemailsController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@user = current_user
-		@customer_email = "duncan.muriithi@meltwater.org"#params[:email] 
+		@customer_email = "duncan.muriithi@meltwater.org" 
 
 		if params[:risklevel] == "risky"
 			@esubject = "EXPIRY NOTICE"
 			puts "I am here to kill you"
-			UserMailer.expiry_notice(@user,@customer_email,@esubject).deliver_now
+			UserMailer.expiry_notice(@user.email,@customer_email,@esubject).deliver_now
 
 		elsif params[:risklevel] == "riskier"
 			@esubject = "EXPIRY NOTICE"
 			puts "I am here to kill you"
-			UserMailer.expiry_notice(@user,@customer_email,@esubject).deliver_now
+			UserMailer.expiry_notice(@user.email,@customer_email,@esubject).deliver_now
 		else
 			@esubject = "EXPIRY NOTICE"
 			puts "I am here to kill you"
-			UserMailer.expiry_notice(@user,@customer_email,@esubject).deliver_now
+			UserMailer.expiry_notice(@user.email,@customer_email,@esubject).deliver_now
 		end
 			
 	end
