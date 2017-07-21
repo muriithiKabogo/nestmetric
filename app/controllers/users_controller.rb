c99536db967669a58adfe6f1d89f3838e7bf288c
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-21 10:32:36 +0000

    merged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
   def index
     @user = current_user
-<<<<<<< HEAD
     # GetRiskyCustomersJob.perform_later(@user)   
-=======
+
     #GetRiskyCustomersJob.perform_later(@user)   
->>>>>>> 2fa574e6993f0694e8e074dc8bab361b655e6f27
+
   	@email_template = EmailTemplate.new
   	@email_templates = @user.email_templates.all
     # @allfailed  = failed_charges_last_30_days.length
