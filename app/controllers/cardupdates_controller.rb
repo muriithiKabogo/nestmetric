f1c4fdece07f5fa327f0342eaa65bca9be2293b6
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-22 12:17:14 +0000

    corrections

--- app/controllers/cardupdates_controller.rb
+++ app/controllers/cardupdates_controller.rb
@@ -1,7 +1,7 @@
 class CardupdatesController < ApplicationController
 	
 	def edit
-		@user = User.find_by(uid: params[:uid])
+		@user = User.find_by_uid(params[:uid])
 		sign_in(:user, @user)
 		redirect_to new_riskycustomer_path
 	end
