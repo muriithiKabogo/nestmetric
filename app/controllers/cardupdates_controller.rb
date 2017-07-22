1c557a242b596b8165fb31ef463c1c598a682139
muriithiKabogo <dmuriithi.k@gmail.com>
2017-07-22 12:19:36 +0000

    credit card update

--- app/controllers/cardupdates_controller.rb
+++ app/controllers/cardupdates_controller.rb
@@ -2,7 +2,7 @@ class CardupdatesController < ApplicationController
 	
 	def edit
 		@user = User.find_by_uid(params[:uid])
-		sign_in(:user, @user)
+		sign_in(@user)
 		redirect_to new_riskycustomer_path
 	end
 end
