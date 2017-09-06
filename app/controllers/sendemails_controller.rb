674cc44ebaf6417472c4d94b1a8658ff8d82c8f9
muriithiKabogo <dmuriithi.k@gmail.com>
2017-09-06 09:47:53 +0000

    test emails

--- app/controllers/sendemails_controller.rb
+++ app/controllers/sendemails_controller.rb
@@ -6,35 +6,63 @@ class SendemailsController < ApplicationController
 		@customer = @user.riskycustomers.find_by(email: @customer_email)
 		
 
-		if params[:risklevel] == "risky"
+		if params[:risklevel] == "thirty days expiry notice"
 			@esubject = "EXPIRY NOTICE"
 			@template = @user.email_templates.find_by_etype("thirty days expiry notice")
 			puts "I am here to kill you"
-			plan = @customer.plan
-			last4 = @customer.last4
+			plan = "pro"
+			last4 = "4567"
 			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
-			@customer.emailsent = true
-			@customer.save
+		
 
-		elsif params[:risklevel] == "riskier"
+		elsif params[:risklevel] == "Fifteen days expiry notice"
 			@esubject = "EXPIRY NOTICE"
 			@template = @user.email_templates.find_by_etype("Fifteen days expiry notice")
 			puts "I am here to kill you"
-			plan = @customer.plan
-			last4 = @customer.last4
+			plan = "pro"
+			last4 = "4568"
 			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
-			@customer.emailsent = true
-			@customer.save
 			
-		else
+			
+		elsif params[:risklevel] == "seven day expiry"
 			@esubject = "EXPIRY NOTICE"
 			@template = @user.email_templates.find_by_etype("seven day expiry")
-			plan = @customer.plan
-			last4 = @customer.last4
+			plan = "pro"
+			last4 = "4569"
 			puts "I am here to kill you"
 			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
-			@customer.emailsent = true
-			@customer.save
+
+		elsif params[:risklevel] == "cancellation"
+			@esubject = "cancellation"
+			@template = @user.email_templates.find_by_etype("cancellation")
+			plan = "pro"
+			last4 = "4559"
+			puts "I am here to kill you"
+			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
+
+		elsif params[:risklevel] == "third attempt"
+			@esubject = "cancellation"
+			@template = @user.email_templates.find_by_etype("third attempt")
+			plan = "pro"
+			last4 = "4569"
+			puts "I am here to kill you"
+			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
+		
+		elsif params[:risklevel] == "second attempt"
+			@esubject = "second attempt"
+			@template = @user.email_templates.find_by_etype("second attempt")
+			plan = "pro"
+			last4 = "4579"
+			puts "I am here to kill you"
+			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
+
+		else
+			@esubject = "first attempt"
+			@template = @user.email_templates.find_by_etype("first attempt")
+			plan = "pro"
+			last4 = "4589"
+			puts "I am here to kill you"
+			UserMailer.expiry_notice(plan, last4, @template,@user.email,@customer_email,@esubject).deliver_now
 		end
 			
 	end
